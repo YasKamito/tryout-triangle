f6ffec5eb241e2161710064fefa15624d94b7849
Yas Kamitokusari <you@example.com>
2017-08-29 23:20:50 +0900

    Change to be able to run on the command line.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,17 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe "Equilateral triangle" do
-  specify { expect(Triangle.check_triangle(1,1,1)).to eq 1 }
+  specify { expect(Triangle.check_triangle(1,1,1)).to eq("正三角形ですね！") }
 end
 
 describe "Isosceles triangle" do
-  specify { expect(Triangle.check_triangle(2,2,1)).to eq 2 }
+  specify { expect(Triangle.check_triangle(2,2,1)).to eq("二等辺三角形ですね！") }
+  specify { expect(Triangle.check_triangle(2,2,3)).to eq("二等辺三角形ですね！") }
+  specify { expect(Triangle.check_triangle(2,2,4)).to eq("三角形じゃないです＞＜") }
 end
 
 describe "Scelene triangle" do
-  specify { expect(Triangle.check_triangle(3,2,4)).to eq 3 }
+  specify { expect(Triangle.check_triangle(3,2,4)).to eq("不等辺三角形ですね！") }
+  specify { expect(Triangle.check_triangle(3,2,5)).to eq("三角形じゃないです＞＜") }
 end
 
 describe "Not triangle" do
-  specify { expect(Triangle.check_triangle(3,2,1)).to eq -1 }
+  specify { expect(Triangle.check_triangle(3,2,1)).to eq("三角形じゃないです＞＜") }
 end
--- triangle.rb
+++ triangle.rb
@@ -2,19 +2,15 @@ class Triangle
   def self.check_triangle(side_1, side_2, side_3)
     array = [side_1, side_2, side_3]
     array.sort!
-
+    p array
     if isEquilateral?(array) then
-        p "正三角形ですね！"
-        return 1
+        return "正三角形ですね！"
     elsif isIsosceles?(array) then
-        p "二等辺三角形ですね！"
-        return 2
+        return "二等辺三角形ですね！"
     elsif isScalene?(array) then
-        p "不等辺三角形ですね！"
-        return 3
+        return "不等辺三角形ですね！"
     else
-        p "三角形ではないです"
-        return -1
+        return "三角形じゃないです＞＜"
     end
     
   end
@@ -30,7 +26,9 @@ class Triangle
   
   # 二等辺三角形チェック
   def self.isIsosceles?(array)
-    if array[1] == array[2] && (array[1] + array[2]) > array[0] then
+    if array[0] == array[1] && (array[0] + array[1]) > array[2] then
+        return true
+    elsif array[1] == array[2] && (array[1] + array[2]) > array[0] then
         return true
     else
         return false
@@ -47,3 +45,6 @@ class Triangle
   end
 
 end
+
+puts(Triangle.check_triangle(ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i))
+
